162270be2be09721d92814dfb2629966d58f6526
gadiroff <53742739+gadiroff@users.noreply.github.com>
2020-01-21 17:54:54 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -52,19 +52,73 @@ variable "pguser" {
   default = "admin"
 }
 
-variable "pghost" {
-  default = "localhost"
-}
+
 
 variable "pgpassword" {
   default = "admin"
 }
 
-variable "pgdatabase" {
-  default = "testt"
-}
 
 variable "pgport" {
   default = "5431"
+}
+
+
+
+##################
+### Postgre RD ###
+##################
+
+
+# Required Variables #
+
+
+variable "aws_ec2_zone_id" {
+  description = "Route53 ZoneID to manage postgres records"
+}
+
+variable "domain" {
+  description = "Domain to use for DNS entries"
+}
+
+
+variable "postgres_version" {
+  description = "Postgres version to deploy"
+  default = "11.5"
+}
+
+
+
+# Optional Variables #
+
+variable "allocated_storage" {
+  description = "Size of the main EBS volume to use for redis nodes"
+  default     = 20
+}
+
+variable "instance_class" {
+  description = "Instance type for postgres nodes"
+  default     = "db.t2.micro"
+}
+
+variable "multi_az" {
+  description = "Setup the DB as multi-AZ (it will have read-only replicas)"
+  default     = true
+}
+
+
+variable "publicly_accessible" {
+  description = "Whether this DB should be publicaly accessible or not"
+  default     = false
+}
+
+variable "storage_encrypted" {
+  description = "Encrypt the DB"
+  default     = false
+}
+
+variable "storage_type" {
+  description = "Type of the data block device for redis"
+  default     = "gp2"
 }
 
