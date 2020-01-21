0878dbdc1500d2ed2cd1b8d4449e3e6d48380989
gadiroff <53742739+gadiroff@users.noreply.github.com>
2020-01-21 17:52:29 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,41 @@
 output "base_url" {
   value = "${aws_api_gateway_deployment.main.invoke_url}"
 }
+
+
+output "api_key" {
+  value = "${aws_api_gateway_api_key.main.id}"
+}
+
+
+##################
+### Postgre RD ###
+##################
+
+output "address" {
+  value = "${aws_db_instance.postgres.address}"
+}
+
+output "arn" {
+  value = "${aws_db_instance.postgres.arn}"
+}
+
+output "availability_zone" {
+  value = "${aws_db_instance.postgres.availability_zone}"
+}
+
+output "id" {
+  value = "${aws_db_instance.postgres.id}"
+}
+
+output "multi_az" {
+  value = "${aws_db_instance.postgres.multi_az}"
+}
+
+output "port" {
+  value = "${aws_db_instance.postgres.port}"
+}
+
+output "status" {
+  value = "${aws_db_instance.postgres.status}"
+}
